537796cc39dc0be90eb14acc004d86077d3911ba
drobiss <brezinadavid93@icloud.com>
2024-09-28 17:00:01 +0200

    *

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -4,12 +4,18 @@ import "./Pricing.css"
 
 const PricingPage = () => {
   const services = [
-    { name: 'Výměna oleje a filtrů', price: '1 500 Kč' },
-    { name: 'Oprava brzd', price: 'od 2 000 Kč' },
-    { name: 'Výměna spojky', price: 'od 5 000 Kč' },
-    { name: 'Údržba a oprava klimatizace', price: 'od 1 800 Kč' },
-    { name: 'Diagnostika vozu', price: '800 Kč' },
-    { name: 'Leštění světlometů', price: 'od 500 Kč' }
+    { name: 'Čištění + plnění klimatizace chladivem R134A', price: 'Odsátí starého chladiva a oleje, vakuování, základní test těsnosti systému, naplnění novým olejem a chladivem 500,- Kč. cena za 1gr. chladiva R134a.....3 Kč.' },
+    { name: 'Čištění + plnění klimatizace chladivem 1234yf', price: 'Odsátí starého chladiva a oleje, vakuování, základní test těsnosti systému, naplnění novým olejem a chladivem 500,- Kč, cena za 1gr. chladiva R1234yf.....4,50 Kč.' },
+    { name: 'Detekce úniku chladiva dusíkem', price: '400,- Kč, při následné opravě v našem servisu 200,- Kč.' },
+    { name: 'Dezinfekce ozónem', price: 'osobní automobil 350,- Kč. dodávka, mikrobus 450,- Kč.' },
+    { name: 'Výměna oleje + filtru motoru', price: '300,- Kč. při zakoupení materiálu v našem servisu, 500,- Kč. při dodaném materiálu' },
+    { name: 'Diagnostika', price: '250,- Kč.' },
+    { name: 'Výměna brzdových destiček + kotoučů', price: '500,- Kč. při zakoupení materiálu v našem servisu' },
+    { name: 'Výměna rozvodového řemene', price: '1000,- Kč. až 2000,- Kč. dle typu automobilu (např. Škoda Octávia 1,9Tdi 1500,- Kč)' },
+    { name: 'Leštění předních světlometů automobilů', price: '500,- Kč. a výše za 1 světlomet dle velikosti světlometu (např. Škoda Fábia 500,- Kč za jeden světlomet)' },
+    { name: 'Zajištění STK + emisí - benzín', price: '3300,- Kč.' },
+    { name: 'Zajištění STK + emisí - nafta', price: '3500,- Kč.' },
+    { name: 'Ceny jsou s včetně DPH.', price: '* Uvedené ceny jsou pouze orientační' },
   ];
 
   return (
